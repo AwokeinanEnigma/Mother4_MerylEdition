731214906148a28206f4ff1306f4e5883b120b11
AwokeinanEnigma <kurogames7595@protonmail.com>
2022-10-07 18:59:16 -0400

    change script data location

--- Mother4/Scripts/ScriptLoader.cs
+++ Mother4/Scripts/ScriptLoader.cs
@@ -46,6 +46,6 @@ namespace Mother4.Scripts
 			return result;
 		}
 
-		private static readonly string SCRIPT_FILE = Paths.RESOURCES + "script.dat";
+		private static readonly string SCRIPT_FILE = Paths.DATA + "script.dat";
 	}
 }
